1f64032f13447cf5bbff6d7576feb88370cef345
Kaiya Kawaguchi <kay914@gmail.com>
2012-08-09 21:08:15 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GodofmealWeb::Application.config.secret_token = '8f0988af49bc674cca422a0ea39c993192d5946e1629280e059e080502faa6a4df8f178bf6814c5aa9afc40521e1da799036763b6f0b97f8090def707a996be2'
