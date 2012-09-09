aa15427673694f7ed410e9435b0ed59ad7c53900
Kaiya Kawaguchi <kay914@gmail.com>
2012-09-10 01:05:07 +0900

    add search

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,7 +1,15 @@
 class RestaurantsController < ApplicationController
   def index
+    @restaurant= {}
   end
 
   def search
+    # @id = params[:search_keys][:id]
+    @id = '1111' 
+    # @latitude = params[:search_keys][:latitude]
+    @latitude = '35.703350' 
+    # @longitude = params[:search_keys][:longitude]
+    @longitude = '139.770736'
+    @restaurant = Restaurant.restaurant_search(@id,@latitude,@longitude)
   end
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,27 @@
 # -*- encoding: utf-8 -*-
 class Restaurant < ActiveResource::Base
-  
+  self.site = "https://godofmeal.herokuapp.com"
+  class Format
+    include ActiveResource::Formats::JsonFormat
+    def decode(json)
+      super(json)
+    end
+  end
+
+  self.format = Format.new
+  def self.restaurant(id,latitude,longitude)
+    self.find(:one, from: "/api/restaurant/search", params: { terminal_id: id, latitude: latitude, longitude: longitude} )
+  end
+
+  def self.restaurant_search(id,latitude,longitude)
+    rs = restaurant(id,latitude,longitude)
+    list = Hash::new
+    list['name'] = rs.name
+    list['category'] = rs.category
+    list['mobile'] = rs.url_mobile
+    list['address'] = rs.address
+    list['tel'] = rs.tel
+    list['image'] = rs.image_url
+    return list
+  end
 end
