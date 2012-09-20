2ba6562ad0c5ae88c981f1d7d1d48a7909aa2885
Kaiya Kawaguchi <kay914@gmail.com>
2012-09-20 23:34:36 +0900

    faster

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,27 +1,19 @@
 # -*- encoding: utf-8 -*-
 class Restaurant < ActiveResource::Base
   self.site = "https://godofmeal.herokuapp.com"
-  class Format
-    include ActiveResource::Formats::JsonFormat
-    def decode(json)
-      super(json)
-    end
-  end
 
-  self.format = Format.new
-  def self.restaurant(id,latitude,longitude)
-    self.find(:one, from: "/api/restaurant/search", params: { terminal_id: id, latitude: latitude, longitude: longitude} )
+  self.format = :json
+
+  def self.restaurant(id, latitude, longitude)
+    self.find(:one, from: "/api/restaurant/search",
+              params: { terminal_id: id,
+                        latitude: latitude,
+                        longitude: longitude })
   end
 
   def self.restaurant_search(id,latitude,longitude)
     rs = restaurant(id,latitude,longitude)
-    list = Hash::new
-    list['name'] = rs.name
-    list['category'] = rs.category
-    list['mobile'] = rs.url_mobile
-    list['address'] = rs.address
-    list['tel'] = rs.tel
-    list['image'] = rs.image_url
-    return list
+    {name: rs.name, category: rs.category, mobile: rs.url_mobile,
+     address: rs.address, tel: rs.tel, image: rs.image_url}
   end
 end
